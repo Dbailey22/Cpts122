92c0c8f3ae832a748a4a09b9692907df5d6baf0d
Daniel Bailey <101297046+Dbailey22@users.noreply.github.com>
2022-05-05 12:10:30 -0700

    Add files via upload

---
+++ StackTemplate/TestStack.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Stack.h"
+
+template <class T>
+class TestStack
+{
+public:
+	TestStack();
+	~TestStack();
+	bool Test(T &item1, T &item2);
+
+private:
+	Stack<T> theStack;
+};
+
+template <class T>
+TestStack<T>::TestStack() : theStack(0)
+{
+
+}
+
+template <class T>
+TestStack<T>::~TestStack()
+{
+
+}
+
+template <class T>
+bool TestStack<T>::Test(T &item1, T &item2)
+{
+	theStack.push(item1);
+	theStack.peek(item2);
+	cout << "peek: " << item2 << endl;
+
+	return true;
+}
